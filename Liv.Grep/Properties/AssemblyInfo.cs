d628d212ca7e8fd41bca10d272982535882b1c15
Livshitz <eli.livshitz@gmail.com>
2014-11-19 09:59:39 +0200

    Adding nuget output

--- Liv.Grep/Properties/AssemblyInfo.cs
+++ Liv.Grep/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Liv.Grep")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Grep utility written in c#. Makes it easy to query command line outputs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Livshitz")]
 [assembly: AssemblyProduct("Liv.Grep")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Eli Livshitz 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
